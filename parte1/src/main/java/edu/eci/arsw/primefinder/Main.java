134b4c411b1e7e28ef1e72b03dc0e8b8cf845a59
Jeimy Yaya <jeimy.yaya-m@mail.escuelaing.edu>
2025-08-23 08:26:45 -0500

    Primer commit

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -3,12 +3,16 @@ package edu.eci.arsw.primefinder;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
-	}
+		int max = 500000000;
+        int part = max / 3;
+
+        PrimeFinderThread pft1 = new PrimeFinderThread(0, part);
+        PrimeFinderThread pft2 = new PrimeFinderThread(part + 1, 2 * part);
+        PrimeFinderThread pft3 = new PrimeFinderThread(2 * part + 1, max);
+
+        pft1.start();
+        pft2.start();
+        pft3.start();
+    }
 	
 }
